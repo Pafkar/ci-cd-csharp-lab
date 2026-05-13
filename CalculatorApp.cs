5f01a80ce6071849ff9fa15fab5e5ac53c79f610
Pavel <pasha2007svs@gmail.com>
2026-05-13 21:11:34 +0300

    Добавьте файлы проекта.

---
+++ CalculatorApp.cs
@@ -0,0 +1,14 @@
+﻿namespace CalculatorApp 
+{
+    internal class CalculatorApp
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] ars)
+            {
+                Calculator calc= new Calculator();
+                Console.WriteLine(calc.Add(2, 3));
+            }
+        }
+    }
+}
